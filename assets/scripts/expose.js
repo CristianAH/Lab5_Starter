868ac89a9f0364cae229d33d645a77e620833385
CristianAH <93352660+CristianAH@users.noreply.github.com>
2022-10-30 22:12:47 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,85 @@
-// expose.js
+// explore.js
 
 window.addEventListener('DOMContentLoaded', init);
 
+var talk;
+var but;
+const synth = window.speechSynthesis;
+var voices = [];
+var currVoice;
+
 function init() {
   // TODO
+  loadVoices();
+}
+
+function loadVoices()
+{
+  voices = synth.getVoices();
+
+  if(voices.length === 0)
+  {
+    setTimeout(function(){ loadVoices(); }, 1000);
+  }
+  else
+  {
+    for (let i = 0; i < voices.length; i++)
+    {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+     }
+    talk = document.getElementById('voice-select');
+    but = document.querySelector("button");
+    but.addEventListener('click', pressTalk);
+    talk.addEventListener('change', voiceChange);
+    
+    console.log('DOM fully loaded and parsed');
+  }
+}
+
+function pressTalk()
+{
+  if(currVoice == null)
+  {
+    alert("Voice is not selected!");
+  }
+  else
+  {
+    var inputText = document.getElementById("text-to-speak").value;
+    const utterance1 = new SpeechSynthesisUtterance(inputText);
+    utterance1.voice = voices[currVoice];
+    synth.speak(utterance1);
+    document.querySelector("#explore img").src = "assets/images/smiling-open.png";
+    document.querySelector("#explore img").alt = "Talking face";
+    checkSpeaking();
+  }
+}
+function checkSpeaking()
+{
+  if(synth.speaking)
+  {
+    setTimeout(function(){ checkSpeaking(); }, 100);
+  }
+  else
+  {
+    document.querySelector("#explore img").src = "assets/images/smiling.png";
+    document.querySelector("#explore img").alt = "Smiling face";
+  }
+}
+
+function voiceChange()
+{
+  for(var i = 0; i < voices.length; i++)
+  {
+    if(talk.value == `${voices[i].name} (${voices[i].lang})`)
+    {
+      currVoice = i;
+      break;
+    }
+  }
 }
